f9a22766dabb87d0f89796883485ba4b9e43e835
Alexey Yatskovets <peacemo@fastmail.fm>
2016-01-22 22:00:05 +0300

    Added contact

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+
+    def __init__(self, name, middle_name, last_name, nick_name, title, company, address, home_phone, mobile_phone,
+                 work_phone, fax_phone, email_1, email_2, email_3, homepage, birthday_year, anniversary_year, address_2,
+                 phone_2, notes):
+        self.name = name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax_phone = fax_phone
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birthday_year = birthday_year
+        self.anniversary_year = anniversary_year
+        self.address_2 = address_2
+        self.phone_2 = phone_2
+        self.notes = notes

e500378f3af3efb0c966077acc9c02d405afeb2a
Alexey Yatskovets <peacemo@fastmail.fm>
2016-01-29 17:02:35 +0300

    Created SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,22 +7,11 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost:8989/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -127,10 +117,6 @@ class Application:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,13 +12,13 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_contact(Contact(name="name", middle_name="middleName", last_name="lastName", nick_name="nickName",
                                  title="title", company="company", address="address", home_phone="homePhone",
                                  mobile_phone="mobilePhone", work_phone="workPhone", fax_phone="faxPhone",
                                  email_1="email1@company", email_2="email2@company.com", email_3="email3@company.com",
                                  homepage="www.homepage.com", birthday_year="2000", anniversary_year="2005",
                                  address_2="address", phone_2="home", notes="notes"))
-    app.logout()
+    app.session.logout()
 
 

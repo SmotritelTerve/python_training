ba995b5226f380ad4cc4d757f7bb913e892019b4
Alexey Yatskovets <peacemo@fastmail.fm>
2016-02-07 17:48:07 +0300

    Added 2 new modify group tests
    use IF statement

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -8,3 +8,15 @@ def test_modify_first_group(app):
     app.group.modify_first_group(Group(name="group" + random_value, header="header" + random_value,
                                        footer="footer" + random_value))
     app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()

40018edcfb8b27deaabc1b2039d79fd31ea321e0
Alexey Yatskovets <peacemo@fastmail.fm>
2016-03-04 12:54:31 +0300

    Task #16
    Задание №16: Реализовать опции командной строки, позволяющие указывать
    тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+# from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,13 +8,23 @@ import random
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        # self.wd = WebDriver()
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
         self.open_home_page()
         # self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +35,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8989/addressbook/")
+        wd.get(self.base_url)
 
     def go_to_home(self):
         wd = self.wd

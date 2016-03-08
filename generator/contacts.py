4fb2cfc2fe53a300232d3e870b749c3df5168dcd
Alexey Yatskovets <peacemo@fastmail.fm>
2016-03-08 22:33:26 +0300

    Task #18 (jsonpickle)
    Задание №18: Реализовать генераторы тестовых данных
    (jsonpickle)

--- generator/contacts.py
+++ generator/contacts.py
@@ -3,7 +3,8 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+# import json
+import jsonpickle
 import getopt
 import sys
 
@@ -60,4 +61,7 @@ testdata = [Contact(first_name="", middle_name="", last_name="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    # out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+

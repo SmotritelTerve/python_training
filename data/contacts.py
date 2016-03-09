03e272844b252011836c71e7b3d96541f26e0d21
Alexey Yatskovets <peacemo@fastmail.fm>
2016-03-09 17:25:33 +0300

    Task #19
    Задание №19: Реализовать загрузку тестовых данных из файлов

--- data/contacts.py
+++ data/contacts.py
@@ -1,44 +1,46 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import random
-import string
+# import random
+# import string
 
-constant = [
-    Contact(first_name="", middle_name="", last_name=""),
+testdata = [
     Contact(first_name="First", middle_name="Firstovich", last_name="Firstoff"),
     Contact(first_name="Second", middle_name="Secondovich", last_name="Secondoff")
 ]
 
+empty_testdata = [
+    Contact(first_name="", middle_name="", last_name="")
+]
 
-def random_string(prefix, maxlen):
-    # symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    # symbols = string.ascii_letters + string.digits + " "*10
-    # symbols = string.ascii_letters
-    symbols = string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-        Contact(first_name=random_string("name", 10),
-                middle_name=random_string("middleName", 10),
-                last_name=random_string("lastName", 10),
-                nick_name=random_string("nickName", 10),
-                title=random_string("title", 10),
-                company=random_string("company", 10),
-                address=random_string("address", 10),
-                home_phone=random_string("homePhone", 10),
-                mobile_phone=random_string("mobilePhone", 10),
-                work_phone=random_string("workPhone", 10),
-                fax_phone=random_string("faxPhone", 10),
-                email_1=random_string("email1@company", 10),
-                email_2=random_string("email2@company.com", 10),
-                email_3=random_string("email3@company.com", 10),
-                homepage=random_string("www.homepage.com", 10),
-                birthday_year=random_string("2000", 10),
-                anniversary_year=random_string("2005", 10),
-                address_2=random_string("address", 10),
-                phone_2=random_string("home", 10),
-                notes=random_string("notes", 10)
-                )
-        for i in range(5)
-    ]
+# def random_string(prefix, maxlen):
+#     # symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+#     # symbols = string.ascii_letters + string.digits + " "*10
+#     # symbols = string.ascii_letters
+#     symbols = string.digits
+#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#
+#
+# testdata = [
+#         Contact(first_name=random_string("name", 10),
+#                 middle_name=random_string("middleName", 10),
+#                 last_name=random_string("lastName", 10),
+#                 nick_name=random_string("nickName", 10),
+#                 title=random_string("title", 10),
+#                 company=random_string("company", 10),
+#                 address=random_string("address", 10),
+#                 home_phone=random_string("homePhone", 10),
+#                 mobile_phone=random_string("mobilePhone", 10),
+#                 work_phone=random_string("workPhone", 10),
+#                 fax_phone=random_string("faxPhone", 10),
+#                 email_1=random_string("email1@company", 10),
+#                 email_2=random_string("email2@company.com", 10),
+#                 email_3=random_string("email3@company.com", 10),
+#                 homepage=random_string("www.homepage.com", 10),
+#                 birthday_year=random_string("2000", 10),
+#                 anniversary_year=random_string("2005", 10),
+#                 address_2=random_string("address", 10),
+#                 phone_2=random_string("home", 10),
+#                 notes=random_string("notes", 10)
+#                 )
+#         for i in range(5)
+#     ]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,13 @@
 from model.contact import Contact
 import pytest
 # from data.contacts import constant as testdata
-from data.contacts import testdata
+# from data.contacts import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+# @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+# def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     # new_contacts = app.contact.get_contact_list()
